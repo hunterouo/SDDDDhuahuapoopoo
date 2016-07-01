6f793efb7b7abb770596e3963fbd6b5ec759d524
wang <hunterwang0917@gmail.com>
2016-07-01 15:37:37 +0800

    0701new

--- app/src/main/java/com/example/wang/huntergod/AddCatActivity.java
+++ app/src/main/java/com/example/wang/huntergod/AddCatActivity.java
@@ -1,51 +1,33 @@
 package com.example.wang.huntergod;
 
-import android.content.ContentResolver;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
-
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import android.widget.Toast;
 
 public class AddCatActivity extends AppCompatActivity {
-    private ImageView mImg;
-    private DisplayMetrics mPhone;
-    private final static int CAMERA = 66 ;
-    private final static int PHOTO = 99 ;
+    private static int RESULT_LOAD_IMG = 1;
+    String imgDecodableString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_cat);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        mImg = (ImageView) findViewById(R.id.imageView);
 
-        Button mPhoto = (Button) findViewById(R.id.button_choose);
-       //Button mPhoto = (Button) findViewById(R.id.photo);
-        mPhoto.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                //開啟相簿相片集，須由startActivityForResult且帶入requestCode進行呼叫，原因點選相片後返回程式呼叫onActivityResult
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent, PHOTO);
-            }
-        });
+
+
+
 
         Spinner spinner4 = (Spinner)findViewById(R.id.spinner4);
 
@@ -63,62 +45,52 @@ public class AddCatActivity extends AppCompatActivity {
 
         spinner4.setAdapter(adapter);
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,Intent data)
-    {
-        //藉由requestCode判斷是否為開啟相機或開啟相簿而呼叫的，且data不為null
-        if ((requestCode == CAMERA || requestCode == PHOTO ) && data != null)
-        {
-            //取得照片路徑uri
-            Uri uri = data.getData();
-            //ContentResolver cr = this.getContentResolver();
-            InputStream inputStream;
-            try
-            {
-                inputStream = getContentResolver().openInputStream(uri);
-                //讀取照片，型態為Bitmap
-                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
-
-                //判斷照片為橫向或者為直向，並進入ScalePic判斷圖片是否要進行縮放
-               /* if(bitmap.getWidth()>bitmap.getHeight())ScalePic(bitmap,
-                        mPhone.heightPixels);
-                else ScalePic(bitmap,mPhone.widthPixels);*/
-            }
-            catch (FileNotFoundException e)
-            {
-            }
-        }
-
-        super.onActivityResult(requestCode, resultCode, data);
+    public void loadImagefromGallery(View view) {
+        // Create intent to Open Image applications like Gallery, Google Photos
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
     }
 
-    /*private void ScalePic(Bitmap bitmap, int phone)
-    {
-        //縮放比例預設為1
-        float mScale = 1 ;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
 
-        //如果圖片寬度大於手機寬度則進行縮放，否則直接將圖片放入ImageView內
-        if(bitmap.getWidth() > phone )
-        {
-            //判斷縮放比例
-            mScale = (float)phone/(float)bitmap.getWidth();
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
 
-            Matrix mMat = new Matrix() ;
-            mMat.setScale(mScale, mScale);
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
 
-            Bitmap mScaleBitmap = Bitmap.createBitmap(
-                    bitmap,
-                    0,
-                    0,
-                    bitmap.getWidth(),
-                    bitmap.getHeight(),
-                    mMat,
-                    false);
-            mImg.setImageBitmap(mScaleBitmap);
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView imgView = (ImageView) findViewById(R.id.imgView);
+                // Set the Image in ImageView after decoding the String
+                imgView.setImageBitmap(BitmapFactory
+                        .decodeFile(imgDecodableString));
+
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
         }
-        else mImg.setImageBitmap(bitmap);
-    }*/
+
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
