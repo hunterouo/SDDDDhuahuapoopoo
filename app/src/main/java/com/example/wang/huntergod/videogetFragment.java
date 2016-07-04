8c827de136422512a71f6f9594fe39e186f34845
wang <hunterwang0917@gmail.com>
2016-07-04 17:10:36 +0800

    0701new

--- app/src/main/java/com/example/wang/huntergod/videogetFragment.java
+++ app/src/main/java/com/example/wang/huntergod/videogetFragment.java
@@ -1,11 +1,13 @@
 package com.example.wang.huntergod;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -23,7 +25,17 @@ public class videogetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_videoget, container, false);
+        View view = inflater.inflate(R.layout.fragment_videoget, container, false);
+        Button button20 = (Button) view.findViewById(R.id.button20);
+        button20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(),AddvideoGetActivity.class);
+
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
 }
