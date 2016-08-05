c0b4814246b592d0eee7615f28399c2f2aad3c2d
wang <hunterwang0917@gmail.com>
2016-08-05 12:31:26 +0800

    0701new

--- app/src/main/java/com/example/wang/huntergod/CleanpooFragment.java
+++ app/src/main/java/com/example/wang/huntergod/CleanpooFragment.java
@@ -1,11 +1,13 @@
 package com.example.wang.huntergod;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -23,7 +25,17 @@ public class CleanpooFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        View view = inflater.inflate(R.layout.fragment_cleanpoo, container, false);
+        Button button20 = (Button) view.findViewById(R.id.button11);
+        button20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(),Video_ChooseActivity.class);
+
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
 }
